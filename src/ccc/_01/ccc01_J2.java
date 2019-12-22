d39224ff064dce8dd73b92654520b4ffd26dcae0
Sadman Hossain <41028402+Sadman-Hossain@users.noreply.github.com>
2019-12-22 12:43:48 -0500

    Complete all CCC '02 Junior problems

--- src/ccc/_01/ccc01_J2.java
+++ src/ccc/_01/ccc01_J2.java
@@ -13,15 +13,10 @@ public class ccc01_J2 {
 	}
 
 	public static void reverseMod(int x, int m) {
-		int a = 0;
-		// int b = 1;
-		while (true) {
-			a += 1;
-			int factor = a * x;
-			for (int b = 1; b <= 100; b++) {
-				if (m * b - factor == 1) {
-					System.out.println(m * b - factor);
-				}
+		int n = m;
+		if (x > 0 && x < m) {
+			for (int i = 1; i < n; i++) {
+				// if (x * i )
 			}
 		}
 	}
