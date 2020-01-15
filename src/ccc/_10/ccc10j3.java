beedd14f5e7478b38e5f9e01043871e8c0873344
Sadman Hossain <41028402+Sadman-Hossain@users.noreply.github.com>
2020-01-14 19:08:11 -0500

    Continue implementing CCC '10 J3

--- src/ccc/_10/ccc10j3.java
+++ src/ccc/_10/ccc10j3.java
@@ -8,19 +8,38 @@ public class ccc10j3 {
 		int A = 0;
 		int B = 0;
 		while (true) {
+			
 			String text = in.nextLine();
-			if (Integer.parseInt(text) == 7 && text.length() == 1) {
+			
+			if (text.length() == 1 && Integer.parseInt(text) == 7) {
 				break;
 			}
+
 			int operation = Integer.parseInt(Character.toString(text.charAt(0)));
-			String var = Character.toString(text.charAt(2));
+			String var1 = Character.toString(text.charAt(2));
+
 			if (operation == 1) {
-				if (var.equals("A")) {
-					A = Integer.parseInt(Character.toString(text.charAt(text.length() - 1)));
+				if (var1.equals("A")) {
+					A = getX(text);
 				} else {
-					B = 
+					B = getX(text);
 				}
+			} else if (operation == 2) {
+				String msg = Character.toString(text.charAt(text.length() - 1));
+				System.out.println(msg);
+			} else if (operation == 3) {
+
 			}
 		}
+	}
+
+	public static int getX(String text, int position) {
+		if (position == 2) {
+			int num = Integer.parseInt(Character.toString(text.charAt(text.length() - 1)));
+		} else if (position == 3) {
+			int num = Integer.parseInt(Character.toString(text.charAt(text.length() - 1)));
+		}
+		
+		return num;
 	}
 }
