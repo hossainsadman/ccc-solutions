c4a1a93cbb56ae64ed69e80be5b9dbfe82f30aac
Sadman Hossain <41028402+Sadman-Hossain@users.noreply.github.com>
2021-01-16 11:52:41 -0500

    start ccc 13 s2

---
+++ src/ccc/_13/ccc13s3.java
@@ -0,0 +1,18 @@
+package ccc._13;
+
+import java.util.*;
+import java.io.*;
+
+public class ccc13s3 {
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(r.readLine());
+        int g = Integer.parseInt(r.readLine());
+
+        for (int i = 0; i < g; i++) {
+            String[] play = r.readLine().split(" ");
+            
+        }
+    }
+}
