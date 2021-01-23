5375f94bf5817b87e8ceb66ec254002e2d6b7512
Sadman Hossain <41028402+Sadman-Hossain@users.noreply.github.com>
2021-01-23 16:14:24 -0500

    completed ccc '20 j4

--- ccc-python/20/20j4.py
+++ ccc-python/20/20j4.py
@@ -1,4 +1,24 @@
+# COMPLETE
+
 t = input()
 s = input()
 
-print(t)
+l = len(s)
+
+shifts = set()
+shifts.add(s)
+
+for i in range(l-1):
+    s = s[1:]+s[0]
+    shifts.add(s)
+
+cyc = False
+for string in shifts:
+    for i in range(0, len(t)-l+1):
+        if string == t[i:i+l]:
+            cyc = True
+
+if cyc == True:
+    print("yes")
+else:
+    print("no")
