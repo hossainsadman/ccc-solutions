1ab0d1d7a59eaf0f6b231208a5f42a5eb364caaa
Sadman Hossain <m.sadman.h@gmail.com>
2021-02-16 19:40:30 -0500

    Add solution for CCC '07 J5

--- python/07/ccc07j5.py
+++ python/07/ccc07j5.py
@@ -1,13 +1,18 @@
 motels = [0, 990, 1010, 1970, 2030, 2940, 3060, 3930, 4060, 4970, 5030, 5990, 6010, 7000]
 
-a = int(input()), b = int(input())
+min = int(input())
+max = int(input())
+
 n = int(input())
-for i in range(n):
-    motels.append(int(input()))
-motels.sort()
+motels = sorted(motels + [int(input()) for i in range(n)])
 
-dists = []
-for i in range(1, len(motels)):
-    dists.append(motels[i]-motels[i-1])
+paths = [0 for i in motels]
+paths[0] = 1
 
-print(dists)
+for i in range(len(motels)):
+    if paths[i] > 0:
+        for j in range(1, len(motels)):
+            if min <= motels[j] - motels[i] <= max:
+                paths[j] += paths[i]
+
+print(paths[-1])
